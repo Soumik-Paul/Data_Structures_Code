6c12bb4d8c2614834cddb3fe0615b7ec23aff4e7
Soumik Paul <146980654+Soumik-Paul@users.noreply.github.com>
2025-08-16 18:33:51 +0600

    Updated code

--- Stack_Queue/parenthesis.cpp
+++ Stack_Queue/parenthesis.cpp
@@ -1,34 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 bool isBalance(string s)
 {
-     cout<<s<<endl;
-     stack<char> st;
-     for(char ch: s)
-     {
-          if(ch=='(' || ch == '{' || ch =='[')
-               st.push(ch);
-          else
-          {
-               if(st.size()==0) // stack is empty
-               return false;
-               char top = st.top();
-               if(ch==')' && top != '(') return false;
-               if(ch=='}' && top!='{') return false;
+    cout << s << endl;
+    stack<char> st;
+    for (char ch : s)
+    {
+        if (ch == '(' || ch == '{' || ch == '[')
+            st.push(ch);
+        else if (ch == ')' || ch == '}' || ch == ']') // only handle closers
+        {
+            if (st.empty()) return false; // stack is empty
+            char top = st.top();
+            if (ch == ')' && top != '(') return false;
+            if (ch == '}' && top != '{') return false;
+            if (ch == ']' && top != '[') return false;
+            st.pop();
+        }
 
-               if(ch==']' && top != '[') return false;
-               st.pop();
-           }
-
-     }
-     return true;
+    }
+    return st.empty(); // যদি stack এ কিছু থেকে থাকে তাইলে False return করবে
 }
-main()
+
+int main()
 {
-     string s= "({})" ;
-     cout<<isBalance(s)<<endl;
-     cout<<isBalance("[][]({})")<<endl;
-     cout<<isBalance("]()")<<endl;
-
-
+    cout << boolalpha;
+    /*
+    The manipulator boolalpha tells cout to instead print them as the words:
+               true
+               false
+*/
+    string s = "({})";
+    cout << isBalance(s) << endl;           // true
+    cout << isBalance("[][]({})") << endl;  // true
+    cout << isBalance("]()") << endl;       // false
+    cout << isBalance("(()") << endl; //false
+    return 0;
 }
