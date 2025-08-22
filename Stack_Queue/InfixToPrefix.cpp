8e404ca9d1fa13df51c4bdd0acfd78dde2787a4c
Soumik Paul <146980654+Soumik-Paul@users.noreply.github.com>
2025-08-22 19:04:52 +0600

    Add files via upload

--- Stack_Queue/InfixToPrefix.cpp
+++ Stack_Queue/InfixToPrefix.cpp
@@ -1,66 +1,80 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int precedence(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
-    return 0;
+int preference (char op)
+{
+     if(op == '+' || op == '-') return 1;
+     if(op == '*' || op=='/') return 2;
+     return 0;
 }
 
-bool isOperator(char c) {
-    return c=='+' || c=='-' || c=='*' || c=='/';
+string InfixToPostfix(string s){
+     string ans;
+     stack<char> st;
+     for (char ch:s )
+     {
+          if(ch== ' ') continue;
+          if(isalnum(ch))
+               ans = ans + ch;
+          else
+          {
+               //1 . Deal with (
+               if(ch=='(')
+                    st.push(ch);
+               else if(ch==')')
+               {
+                    while(st.top() != '(')
+                    {
+                         ans = ans + st.top();
+                         st.pop();
+                    }
+                    st.pop(); // to pop the (
+               }
+               else
+               {
+                    while(!st.empty() && st.top() !='(' && preference(st.top()) > preference(ch))
+                    {
+                          ans = ans + st.top();
+                         st.pop();
+                    }
+                    st.push(ch);
+
+               }
+          }
+     }
+
+     while(st.size()!=0)
+     {
+          ans = ans + st.top();
+          st.pop();
+     }
+     return ans;
+
+
 }
 
-string infixToPostfix(const string& s) {
-    stack<char> st;
-    string out;
-    for (char c : s) {
-        if (c == ' ') continue;                 // ignore spaces
-        if (isalnum(static_cast<unsigned char>(c))) {
-            out += c;                           // operand -> output
-        } else if (c == '(') {
-            st.push(c);
-        } else if (c == ')') {
-            while (!st.empty() && st.top() != '(') {
-                out += st.top(); st.pop();
-            }
-            st.pop();          // pop '('
-        } else if (isOperator(c)) {
-            while (!st.empty() && st.top() != '(' &&
-                   precedence(st.top()) > precedence(c)) {
-                out += st.top(); st.pop();
-            }
-            st.push(c);
-        }
-    }
-    while (!st.empty()) { out += st.top(); st.pop(); }
-    return out;
+string InfixToPrefix(string s)
+{
+     //1 reverse
+     string rev=s;
+     reverse(rev.begin(),rev.end());
+     for(int i =0;i <s.size();i++)
+     {
+          char ch = rev[i];
+          if(ch == '(')
+               rev[i]=')';
+          else if (ch==')')
+               rev[i]= '(';
+     }
+
+     string ans = InfixToPostfix(rev);
+     reverse(ans.begin(),ans.end());
+     return ans;
 }
 
-// Infix -> Prefix: reverse (swap brackets) -> postfix -> reverse
-string infixToPrefix(const string& s) {
-    // 1) Reverse and swap '(' to ')'
-    string rev;
-    rev.reserve(s.size());
-    for (int i = (int)s.size() - 1; i >= 0; --i) {
-        char c = s[i];
-        if (c == '(') rev += ')';
-        else if (c == ')') rev += '(';
-        else rev += c;
-    }
+main()
+{
+     string s= "a+ b * (c-d + e) -f";
+     cout<<InfixToPrefix(s)<<endl;
 
-    // 2) Convert reversed infix to postfix
-    string post = infixToPostfix(rev);
-
-    // 3) Reverse postfix to get prefix
-    reverse(post.begin(), post.end());
-    return post;
 }
-
-int main() {
-    string s = "a + b * (c-d) + e";
-    cout << "Infix  : " << s << "\n";
-    cout << "Prefix : " << infixToPrefix(s) << endl; // + + a * b - c d e
-    return 0;
-}
-
