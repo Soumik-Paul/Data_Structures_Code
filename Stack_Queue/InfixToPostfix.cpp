bc0fceadecf9065d41b690762708da7a9d576010
Soumik Paul <146980654+Soumik-Paul@users.noreply.github.com>
2025-08-22 19:02:12 +0600

    Updated

--- Stack_Queue/InfixToPostfix.cpp
+++ Stack_Queue/InfixToPostfix.cpp
@@ -1,20 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Return precedence of an operator: higher value = higher precedence
 int precedence(char op) {
     if (op == '+' || op == '-') return 1;
     if (op == '*' || op == '/') return 2;
     return 0;   // for '(' or anything else
 }
 
-// Is this character one of our operators?
-bool isOperator(char c) {
-    return c=='+' || c=='-' || c=='*' || c=='/';
-}
-
-
-string infixToPostfix(const string& s) {
+string infixToPostfix(string s) {
     stack<char> st;
     string out;       // result
 
@@ -26,25 +19,24 @@ string infixToPostfix(const string& s) {
         } else if (c == '(') {                  // '(' -> stack
             st.push(c);
         } else if (c == ')') {                 // pop until '('
-            while (!st.empty() && st.top() != '(') {
-                out += st.top(); st.pop();
+            while (st.top() != '(') {
+                out += st.top();
+                st.pop();
             }
              st.pop();          // remove '('
-        } else if (isOperator(c)) {             // operator
-            // While stack top has an operator with >= precedence,
-            // pop it to output. (Left-associative: + - * /)
-            while (!st.empty() && st.top() != '(' &&
-                   precedence(st.top()) >= precedence(c)) {
-                out += st.top(); st.pop();
+        }
+        else {             // operator
+            while (!st.empty() && st.top() != '(' && precedence(st.top()) >= precedence(c)) {
+                out += st.top();
+                st.pop();
             }
             st.push(c);                         // push current operator
         }
-        // Any other character is ignored
     }
 
-    // Pop any remaining operators
     while (!st.empty()) {
-        out += st.top(); st.pop();
+        out += st.top();
+        st.pop();
     }
     return out;
 }
@@ -52,6 +44,9 @@ string infixToPostfix(const string& s) {
 int main() {
     string s = "a + b * (c-d) + e";
     cout << infixToPostfix(s) << endl;
+
+
+
     string s2 = "(x + y) * ((A - b) / C)";
     cout << infixToPostfix(s2) << endl;
     return 0;
